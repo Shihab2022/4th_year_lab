c4e37f178dbfbdf1a08561abdecb9d500df46f02
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2025-12-21 16:54:15 +0600

    complete Jacobi iterative

--- Jacobi-iterative.cpp
+++ Jacobi-iterative.cpp
@@ -1,4 +1,4 @@
-#include<ipstream>
+#include<iostream>
 #include<iomanip>
 #include<cmath>
 using namespace std;
@@ -15,4 +15,69 @@ int main(){
 	double eps,temp;
 	cout<<"\n Enter the elements of the arguments matirx row-wise : \n";
 	for (i=0;i<n;i++)
+	for(j=0;j<=n;j++)
+		cin>>a[i][j];
+	cout<<"\n the matrix you have entered is \n";
+	for(i=0;i<n;i++){
+		for(j=0;j<=n;j++)
+		cout<<a[i][j]<<setw(10);
+		cout<<"\n";
+	}
+	cout<<"\n";
+	cout <<"\n enter the initail values of the variables : \n";
+	for(i=0;i<n;i++)
+	cin>>x[i];
+	cout<<"\n enter the accuracy upto which you want to solution \n";
+	cin>>eps;
+	for(i=0;i<n-1;i++)
+		for(k=i+1;k<n;k++)
+			if(abs(a[i][i])<abs(a[k][i]))
+				for(j=0;j<=n;j++){
+						temp=a[i][j];
+						a[i][j]=a[k][j];
+						a[k][j]=temp;
+				}
+	cout<<"\n The matrix after pivoting \n";
+	cout <<"\n------------------------\n";
+	for(i=0;i<n;i++){
+		for(j=0;j<=n;j++){
+			cout<<a[i][j]<<setw(10);
+		}
+		cout<<"\n";
+	}
+	cout<<"\n";
+	cout<<"\n---------------------------\n";
+	do{
+		for(i=0;i<n;i++){
+			x1[i]=x[i];
+			r[i]=(a[i][n]/a[i][i]);
+			for(j=0;j<n;j++){
+				if(j!=i)
+				r[i]=r[i]-(a[i][j]/a[i][i]*x[j]);
+			}
+		}
+		for(i=0;i<n;i++){
+			x[i]=r[i];
+		}
+	}
+	while(fabs(x1[0]-x[0])>eps || fabs(x1[1]-x[1])>eps ||fabs(x1[2]-x[2])>eps);
+	cout<<"\n The solution is as follows : \n";
+	for(i=0;i<n;i++)
+	cout<<"\n x["<<i<<"]="<<x[i];
+	return 0;
 }
+
+//Enter the no. of equations
+// 3
+// Enter the elements of the augmented matrix row-wise:
+//10 -5 -2 3
+//4 -10 3 -3
+//1 6 10 -3
+//Enter the initial values of the variables:
+// 0 0 0
+//Enter the accuracy upto which you want the solution:
+//.001
+//The solution is as follows:
+//x[0] = 0.3417
+//x[1] = 0.2852
+//x[2] = -0.5052
